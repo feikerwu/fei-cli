f63ed2cf867c6cdec02741c0b7c58c713abbaf32
feikerwu <feikerwu@gmail.com>
2019-07-26 19:55:20 +0800

    feat: add script init

---
+++ src/scripts/fei-init.ts
@@ -0,0 +1,71 @@
+import * as fs from 'fs-extra';
+import path from 'path';
+import ora from 'ora';
+import chalk from 'chalk';
+import { installCmd } from '../utils';
+import execa from 'execa';
+
+console.log(__dirname);
+
+const { Select, Input } = require('enquirer');
+const cwd = process.cwd();
+
+const project: {
+  name: string;
+  template: string;
+} = {
+  name: '',
+  template: ''
+};
+
+export function init(dir?: string) {
+  if (dir) {
+    project.name = dir;
+  }
+
+  const { name } = project;
+  const realProjectPath = path.join(cwd, name);
+  fs.existsSync(realProjectPath)
+    ? handleDirExist()
+    : createProject(realProjectPath);
+}
+
+async function createProject(realProjectPath) {
+  if (!project.template) {
+    const prompt = new Select({
+      message: 'pick a template',
+      choices: fs.readdirSync(path.join(__dirname, '../../templates'))
+    });
+
+    project.template = await prompt.run();
+  }
+
+  const srcDir = path.resolve(__dirname, '../../templates', project.template);
+  const destDir = path.resolve(cwd, realProjectPath);
+
+  let spinner = ora({
+    color: 'yellow',
+    text: 'creating project'
+  }).start();
+  fs.copy(srcDir, destDir).then(
+    async () => {
+      spinner.succeed();
+      process.chdir(destDir);
+      spinner.start('install packages');
+      await execa(installCmd);
+      spinner.succeed();
+    },
+    () => spinner.fail('failed')
+  );
+}
+
+async function handleDirExist() {
+  console.log(chalk.red('project exist!'));
+  const anotherDirName = await new Input({
+    message: 'input another name'
+  }).run();
+
+  project.name = anotherDirName;
+
+  init();
+}
